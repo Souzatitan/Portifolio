851a9abcde7cd100cc25c3759081d298aff026e5
Souzatitan <asouzanderson@gmail.com>
2025-01-03 16:10:10 -0300

    pausestart

--- assets/scripts/carrossel.js
+++ assets/scripts/carrossel.js
@@ -1,8 +1,23 @@
+
 const carrossel = document.querySelector('.carrossel');
 
+if (carrossel) {
+ 
+  const items = Array.from(carrossel.children);
+  items.forEach(item => {
+    const clone = item.cloneNode(true);
+    carrossel.appendChild(clone);
+  });
 
-const items = Array.from(carrossel.children);
-items.forEach(item => {
-  const clone = item.cloneNode(true);
-  carrossel.appendChild(clone);
-});
+ 
+  const pauseAnimation = () => carrossel.classList.add('pause');
+  const resumeAnimation = () => carrossel.classList.remove('pause');
+
+
+  carrossel.addEventListener('mouseover', pauseAnimation);
+  carrossel.addEventListener('mouseout', resumeAnimation);
+
+ 
+  carrossel.addEventListener('touchstart', pauseAnimation);
+  carrossel.addEventListener('touchend', resumeAnimation);
+}
